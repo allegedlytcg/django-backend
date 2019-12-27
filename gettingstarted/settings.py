9aec76975bd26b63a3e1bf29ecd671d4cef12922
charley43613 <41710701+charley43613@users.noreply.github.com>
2019-12-26 20:02:53 -0500

    hopefully we can communicate between apps now

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -53,13 +53,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 
 
 ]
 MIDDLEWARE_CLASSES = (
     #...
-    'python-getting-started.CorsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.middleware.common.CommonMiddleware',
     #...
@@ -81,14 +81,14 @@ TEMPLATES = [
         },
     }
 ]
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = (
-    'https://newpkmtcg.herokuapp.com/',
-    'http//:localhost:8000',
-    '0.0.0.0:5000',
-    'https://hardcore-boyd-45f941.netlify.com/',
-)
+# CORS_ORIGIN_WHITELIST = (
+#     'https://newpkmtcg.herokuapp.com/',
+#     'http//:localhost:8000',
+#     '0.0.0.0:5000',
+#     'https://hardcore-boyd-45f941.netlify.com/',
+# )
 
 WSGI_APPLICATION = "gettingstarted.wsgi.application"
 

c018bef0ddf141903b4e8ce8f118e64f54cf3e22
charley43613 <41710701+charley43613@users.noreply.github.com>
2020-01-18 02:54:36 -0500

    Attempt to register models to admin site for easy view yet a third time

--- cardapp/admin.py
+++ cardapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import CardTransact, Deck
-admin.site.register(CardTransact,Deck)
+admin.site.register(Deck)
+admin.site.register(CardTransact)
 # Register your models here.
